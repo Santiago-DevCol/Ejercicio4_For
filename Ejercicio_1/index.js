e6d60dc4a58273f78abf35732b9f92c73635f2f9
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-06-17 13:17:18 -0500

    solucion ejercicios de la guia

---
+++ Ejercicio_1/index.js
@@ -0,0 +1,8 @@
+for (let index = 1; index <= 50; index++) {
+    
+    if (index % 2 !== 0) {
+        console.log(index);
+    }
+    
+}
+alert("valida la consola")
---
+++ Ejercicio_3/index.js
@@ -0,0 +1,8 @@
+const array =  [4,"dos",8,"tres",5,9,1,"cero"]
+
+for (let index = 0; index < array.length; index++) {
+    if (typeof(array[index]) == "number" ) {
+        console.log(array[index]);
+    } 
+}
+alert("⚠ valida la consola")
